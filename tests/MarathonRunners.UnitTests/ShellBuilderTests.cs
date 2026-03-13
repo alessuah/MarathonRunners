4cc3c69c35a5f5d9af9648f37bfb1e5ca084780a
Alex <lpzalejandroonlaptop@hotmail.com>
2026-03-13 13:35:01 +0100

    feat(project): Add unit tests

---
+++ tests/MarathonRunners.UnitTests/ShellBuilderTests.cs
@@ -0,0 +1,18 @@
+﻿using MarathonRunners.Core;
+
+namespace MarathonRunners.UnitTests;
+
+internal class ShellBuilderTests
+{
+    [Test]
+    public async Task ShellBuilder_Creates_Rook_As_Default_Shell()
+    {
+        //Arrange & Act
+        var rookShell = ShellBuilder.CreateBuilder()
+            .Build();
+
+        //Assert
+        await Assert.That(rookShell.Name).IsEqualTo("Rook");
+
+    }
+}
