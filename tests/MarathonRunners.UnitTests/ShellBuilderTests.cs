6c2d541fb1d33642f997279f4f826330724f8839
Alex <lpzalejandroonlaptop@hotmail.com>
2026-03-13 13:46:00 +0100

    feat(tests): Add ShellBuilderTests

--- tests/MarathonRunners.UnitTests/ShellBuilderTests.cs
+++ tests/MarathonRunners.UnitTests/ShellBuilderTests.cs
@@ -14,5 +14,10 @@ internal class ShellBuilderTests
         //Assert
         await Assert.That(rookShell.Name).IsEqualTo("Rook");
 
+        await Assert.That(rookShell)
+            .Member(r => r.Name, name => name.IsEqualTo("Rook"))
+            .And.Member(r => r.Health, Health => Health.IsEqualTo(100u))
+            .And.Member(r => r.Shield, Shield => Shield.IsEqualTo(200u));
+
     }
 }
